b3e3ff68b799cfe0980ca5fb6d14dcf4cd79de8e
TeddyArellano <100046697+TeddyArellano@users.noreply.github.com>
2023-06-19 23:34:14 -0600

    Descripción del commit

--- app/src/main/java/com/example/proyectodecalculo/CreditsActivity.kt
+++ app/src/main/java/com/example/proyectodecalculo/CreditsActivity.kt
@@ -1,11 +1,19 @@
 package com.example.proyectodecalculo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CreditsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener{
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
